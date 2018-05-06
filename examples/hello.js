289e36ec13108db870cb965cf1952a01c7d65236
ak1103dev <ak.apichan1103@gmail.com>
2018-05-06 17:52:53 +0700

    :lipstick: increase font size

--- examples/hello.js
+++ examples/hello.js
@@ -2,7 +2,7 @@ module.exports = function (inputColor) {
 	return {
 		contentType: 'text/html',
 		content: `<html><body>
-		 <div style="color: ${inputColor.color}">
+		 <div style="color: ${inputColor.color}; font-size: 200%;">
 			สวัสดี
 		 </div>
 		</body></html>`
